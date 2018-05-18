738b0afbc8aea98fb003406d2650ce0f061a6ddc
Greg Rutherford <g.rutherford10@gmail.com>
2018-05-18 17:54:31 +0100

    modified files

---
+++ pet_shop.rb
@@ -0,0 +1,39 @@
+def pet_shop_name(petshop)
+ return petshop[:name]
+end
+
+def total_cash(cash)
+  return cash[:admin][:total_cash]
+end
+
+def add_or_remove_cash(money, payment)
+ total_money = money[:admin][:total_cash] += payment
+ return total_money
+end
+
+def pets_sold(pets)
+ return pets[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet, num)
+ petsSold = pet[:admin][:pets_sold] += num
+ return petsSold
+end
+
+def stock_count(stock)
+ return stock[:pets].length
+end
+
+def pets_by_breed(pets, breed)
+  dogBreed = []
+  for dog in pets[:pets]
+    if dog[:breed] == breed
+      dogBreed.push(dog)
+    end
+  end
+  return dogBreed
+end
+
+def find_pet_by_name()
+
+end 
